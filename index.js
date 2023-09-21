4e2e8eb15c0ad6b2dd2be8484d7d6e1ea8b0b230
Om sharma <73094136+Omsharma1813111032@users.noreply.github.com>
2023-09-21 15:22:00 +0530

    UPdated

---
+++ index.js
@@ -0,0 +1,12 @@
+function change_bck(){
+    var a = document.querySelector(".in").value;
+    var b = document.querySelector("body");
+    b.style.backgroundColor=a;
+}
+
+
+function change_fnt(){
+    var c = document.querySelector(".fnt").value;
+    var d = document.querySelector("body");
+    d.style.color=c;
+}
